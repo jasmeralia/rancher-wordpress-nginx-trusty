f99db779d4b0eb1dbd1dbe5dab53f841ee552f0c
Morgan Blackthorne <morgan@windsofstorm.net>
2016-08-06 11:32:57 -0700

    Remove mysql config and switch to using an external DB via environment variables. Also specify volume only for the wp-content directory, store a copy of wp-config.php there and restore it at bootup.

--- start.sh
+++ start.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
-  #mysql has to be started this way as it doesn't work to call from /etc/init.d
-  /usr/bin/mysqld_safe &
-  sleep 10s
-  # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
-  WORDPRESS_DB="wordpress"
-  MYSQL_PASSWORD=`pwgen -c -n -1 12`
-  WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-  #This is so the passwords show up in logs.
-  echo mysql root password: $MYSQL_PASSWORD
-  echo wordpress password: $WORDPRESS_PASSWORD
-  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-  echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
+if [ ! -f /usr/share/nginx/www/wp-content/wp-config.php ]; then
+  cp /usr/share/nginx/www/wp-content/wp-config.php /usr/share/nginx/www/wp-config.php
+  chown www-data:www-data /usr/share/nginx/www/wp-config.php
+fi
 
-  sed -e "s/database_name_here/$WORDPRESS_DB/
-  s/username_here/$WORDPRESS_DB/
-  s/password_here/$WORDPRESS_PASSWORD/
+if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
+  # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
+  sed -e "s/database_name_here/$WORDPRESS_DB_NAME/
+  s/localhost/$WORDPRESS_DB_HOST/
+  s/username_here/$WORDPRESS_DB_USER/
+  s/password_here/$WORDPRESS_DB_PASS/
   /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
@@ -44,12 +38,8 @@ if ( count( \$plugins ) === 0 ) {
 }
 ENDL
 
-  chown www-data:www-data /usr/share/nginx/www/wp-config.php
-
-  mysqladmin -u root password $MYSQL_PASSWORD
-  mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
-  killall mysqld
+  cp /usr/share/nginx/www/wp-config.php /usr/share/nginx/www/wp-content/wp-config.php
+  chown www-data:www-data /usr/share/nginx/www/wp-config.php  /usr/share/nginx/www/wp-content/wp-config.php
 fi
 
 # start all the services
